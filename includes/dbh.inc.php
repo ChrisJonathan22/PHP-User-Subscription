770b8a426842d8ef2df10ae42bc121f067715a56
Chris Jonathan <christophereko@hotmail.fr>
2019-12-08 13:58:43 +0000

    I've added an if statement to check if the connection to the database was successful.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,4 +7,7 @@ $dbName = 'subscription';
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
+if (!$conn) {
+    die("Connection Failed:" . mysqli_connect_error());
+}
 ?>
